7822670e33c990d2cb1044020bd5f3a2f4b4d25c
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-11-27 15:14:40 -0700

    inversed insertion sort implemented + read me filled

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function insertionSortReverse(array) {
-    return array;
+  //console.log("starting array: " + array)
+    for(var i = (array.length - 2 ); i >= 0; i--) {
+        var val = array[i];
+        var j;
+        for(j = i; j < array.length && val > array[j+1]; j++) {
+          array[j] = array[j+1]
+        }
+        array[j] = val;
+      }
+      return array;
 }
+
+
